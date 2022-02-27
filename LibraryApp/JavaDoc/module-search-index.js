89b8e836a235c3baa3ee946a92221526273e74c0
AlinaVevel <90707109+AlinaVevel@users.noreply.github.com>
2022-02-27 20:03:09 +0100

    JavaDoc

---
+++ LibraryApp/JavaDoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.alinavevel.libraryapp"}];updateSearchResults();
---
+++ LibraryApp/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.alinavevel.libraryapp","l":"com.alinavevel.libraryapp"}];updateSearchResults();
